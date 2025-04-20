f59db87c7930b681aa07da46c6b144d032580a2b
Roshan Gupta <43931122+rookie2410@users.noreply.github.com>
2025-04-20 14:24:36 +0530

    Update sonarqube-user-data.sh

--- jenkins-sonar-nexus/user-data-scripts/sonarqube-user-data.sh
+++ jenkins-sonar-nexus/user-data-scripts/sonarqube-user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-apt-get update -y
-apt-get install -y docker.io
-usermod -aG docker ubuntu
-newgrp docker
+sudo apt-get update -y
+sudo apt-get install -y docker.io
+sudo usermod -aG docker ubuntu
+sudo newgrp docker
 docker run -d -p 9000:9000 sonarqube:lts-community
